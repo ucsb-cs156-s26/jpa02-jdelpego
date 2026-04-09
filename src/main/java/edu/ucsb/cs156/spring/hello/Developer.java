81bb950b292f34b752859b619438f912a2147878
jdelpego <jdelpego@gmail.com>
2026-04-08 18:44:32 -0700

    added my personal info with no tests yet

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,8 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s26-teams>
-        // i.e. your first name, or your first and initial of last name
 
-        return "Chris G.";
+        return "Joaquin D";
     }
 
     /**
@@ -29,8 +26,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "jdelpego";
     }
 
     /**
@@ -39,14 +35,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s26-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s26-05");
+        team.addMember("Joaquin D");
+        team.addMember("Soome");
+        team.addMember("Elissa");
+        team.addMember("Alexia");
+        team.addMember("John K");
+        team.addMember("Ibrahim");
         return team;
     }
 }
